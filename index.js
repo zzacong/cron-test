fe2aefbcf6dab8708fd1f12db380e57e59738811
Zac Ong <61817066+zzacong@users.noreply.github.com>
2021-11-06 22:38:05 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, Intents } = require('discord.js')
+
+process.env.NODE_ENV !== 'production' &&
+  require('dotenv').config({ path: '.env.local' })
+
+const { GUILDS } = Intents.FLAGS
+
+async function main() {
+  const client = new Client({ intents: [GUILDS] })
+
+  await client.login(process.env.DISCORD_BOT_TOKEN)
+  console.log(`Logged in as ${client.user.tag}`)
+
+  const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID)
+  await channel.send(`Hello Discord`)
+
+  return
+}
+
+main().catch(console.log)
